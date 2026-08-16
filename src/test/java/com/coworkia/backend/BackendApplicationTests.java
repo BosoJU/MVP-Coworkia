f8ddc9b5b70577374f481683b892407e31305a9c
Boso <justine.boso@hotmail.com>
2026-08-16 14:37:43 +0200

    Initial commit

---
+++ src/test/java/com/coworkia/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coworkia.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
