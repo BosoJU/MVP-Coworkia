37f5db96d8203b4e7f5dc11d7c9d6a19c9ef4b4c
Boso <justine.boso@hotmail.com>
2026-08-16 22:17:34 +0200

    backend : ajout de UserService et allègement de UserController

--- src/main/java/com/coworkia/backend/controllers/PrivateController.java
+++ src/main/java/com/coworkia/backend/controllers/PrivateController.java
@@ -1,12 +1,7 @@
 package com.coworkia.backend.controllers;
 
-import com.coworkia.backend.configurations.JwtService;
 import com.coworkia.backend.entities.InternalUser;
-import com.coworkia.backend.repository.InternalUserRepository;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import com.coworkia.backend.service.UserService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,40 +9,28 @@ import java.util.List;
 @RestController
 @RequestMapping("api/private")
 public class PrivateController {
-    private final InternalUserRepository internalUserRepository;
-    private JwtService jwtService;
-    private final AuthenticationManager authenticationManager;
-    private final PasswordEncoder passwordEncoder;
+    private final UserService userService;
 
-    public PrivateController(InternalUserRepository internalUserRepository, JwtService jwtService, PasswordEncoder passwordEncoder, AuthenticationManager authenticationManager) {
-        this.internalUserRepository = internalUserRepository;
-        this.jwtService = jwtService;
-        this.authenticationManager = authenticationManager;
-        this.passwordEncoder = passwordEncoder;
+    public PrivateController (UserService userService){
+        this.userService = userService;
     }
-
     @PostMapping("/login")
     public String login (@RequestBody LoginRequest loginRequest){
-        Authentication authentication = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(loginRequest.email(),
-                        loginRequest.password())
-        );
-        return jwtService.generateToken(authentication);
+        return userService.login(loginRequest);
     }
 
     @GetMapping("/getUsers")
     public List<InternalUser> getUser(){
-        return internalUserRepository.findAll();
+        return userService.getUser();
     }
 
     @PostMapping("/addUser")
     public InternalUser CreateUser (@RequestBody InternalUser internalUser){
-        internalUser.setPassword(passwordEncoder.encode(internalUser.getPassword()));
-        return internalUserRepository.save(internalUser);
+        return userService.CreateUser(internalUser);
     }
 
     @DeleteMapping("/{id}")
     public void deledtUser (@PathVariable Long id){
-        internalUserRepository.deleteById(id);
+        userService.deledtUser(id);
     }
 }
---
+++ src/main/java/com/coworkia/backend/service/UserService.java
@@ -0,0 +1,49 @@
+package com.coworkia.backend.service;
+
+import com.coworkia.backend.configurations.JwtService;
+import com.coworkia.backend.controllers.LoginRequest;
+import com.coworkia.backend.entities.InternalUser;
+import com.coworkia.backend.repository.InternalUserRepository;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    private final InternalUserRepository internalUserRepository;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserService(InternalUserRepository internalUserRepository, JwtService jwtService, PasswordEncoder passwordEncoder, AuthenticationManager authenticationManager) {
+        this.internalUserRepository = internalUserRepository;
+        this.jwtService = jwtService;
+        this.authenticationManager = authenticationManager;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public String login (LoginRequest loginRequest){
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginRequest.email(),
+                        loginRequest.password())
+        );
+        return jwtService.generateToken(authentication);
+    }
+
+    public List<InternalUser> getUser(){
+        return internalUserRepository.findAll();
+    }
+
+    public InternalUser CreateUser (InternalUser internalUser){
+        internalUser.setPassword(passwordEncoder.encode(internalUser.getPassword()));
+        return internalUserRepository.save(internalUser);
+    }
+
+    public void deledtUser (Long id){
+        internalUserRepository.deleteById(id);
+    }
+}
