4a69feec42277fb3a972d9d4dedea46ebde42bd5
Boso <justine.boso@hotmail.com>
2026-08-16 22:53:09 +0200

    backend : vérification de la disponibilité de l'email avant la création d'un user

--- src/main/java/com/coworkia/backend/service/UserService.java
+++ src/main/java/com/coworkia/backend/service/UserService.java
@@ -11,6 +11,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -39,6 +40,12 @@ public class UserService {
     }
 
     public InternalUser CreateUser (InternalUser internalUser){
+        Optional<InternalUser> userExistant = internalUserRepository.findByEmail(internalUser.getEmail());
+
+        if(userExistant.isPresent()){
+            throw new IllegalArgumentException("Un utilisateur existe déjà avec cet email" + internalUser.getEmail());
+        }
+
         internalUser.setPassword(passwordEncoder.encode(internalUser.getPassword()));
         return internalUserRepository.save(internalUser);
     }
