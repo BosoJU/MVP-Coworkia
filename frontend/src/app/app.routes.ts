2616998f6ada1e6821f81ad45477aa344a589ed8
Boso <justine.boso@hotmail.com>
2026-08-17 19:34:31 +0200

    frontend : guard de route

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,10 +3,11 @@ import { Zones } from './pages/zones/zones';
 import { Login } from './pages/login/login';
 import { MesReservations } from './pages/mes-reservations/mes-reservations';
 import { Historique } from './pages/historique/historique';
+import { authGuard } from './guards/auth-guard';
 
 export const routes: Routes = [
-    {path:'zones', component: Zones},
-    {path:'login', component: Login},
-    {path:'mes-reservations', component: MesReservations},
-    {path:'historique', component: Historique},
+    {path:'zones', component: Zones, canActivate: [authGuard]},
+    {path:'login', component: Login, canActivate: [authGuard]},
+    {path:'mes-reservations', component: MesReservations, canActivate: [authGuard]},
+    {path:'historique', component: Historique, canActivate:[authGuard]},
 ];
