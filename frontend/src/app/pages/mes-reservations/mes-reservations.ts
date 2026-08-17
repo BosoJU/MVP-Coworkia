3a95fd9007e876cb689347c2cfca7f80784a1b7a
Boso <justine.boso@hotmail.com>
2026-08-17 15:44:29 +0200

    frontend : structure et styling page mes réservations

---
+++ frontend/src/app/pages/mes-reservations/mes-reservations.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mes-reservations',
+  imports: [],
+  templateUrl: './mes-reservations.html',
+  styleUrl: './mes-reservations.css',
+})
+export class MesReservations {}
