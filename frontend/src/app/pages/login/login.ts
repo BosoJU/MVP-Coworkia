bd769000c5a246aa8f5c185b803c98605de0aa39
Boso <justine.boso@hotmail.com>
2026-08-18 00:31:19 +0200

    frontend : connesion de la page de login à l'API

--- frontend/src/app/pages/login/login.ts
+++ frontend/src/app/pages/login/login.ts
@@ -1,9 +1,31 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Auth } from '../../services/auth';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+export class Login {
+  email ='';
+  password= '';
+  messageErreur='';
+
+  constructor(private auth: Auth, private router: Router){}
+
+  onSubmit(): void{
+    this.auth.login(this.email, this.password).subscribe({
+      next: (token) => {
+        localStorage.setItem('token', token);
+        this.router.navigate(['/zones']);
+      }, 
+      error: () => {
+        this.messageErreur = 'Identifiants incorrects';
+      }
+    })
+  }
+
+}
