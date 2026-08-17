0a5bab4329a02aac8aa9f994d037e279967325c0
Boso <justine.boso@hotmail.com>
2026-08-17 15:44:44 +0200

    frontend : structure et styling page zones

---
+++ frontend/src/app/pages/zones/zones.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-zones',
+  imports: [],
+  templateUrl: './zones.html',
+  styleUrl: './zones.css',
+})
+export class Zones {
+}
