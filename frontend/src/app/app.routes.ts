5c83a2bfee79504c31806b7767b370e1c0bc0657
Boso <justine.boso@hotmail.com>
2026-08-17 15:45:09 +0200

    frontend : routing

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Zones } from './pages/zones/zones';
+import { Login } from './pages/login/login';
+import { MesReservations } from './pages/mes-reservations/mes-reservations';
+import { Historique } from './pages/historique/historique';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'zones', component: Zones},
+    {path:'login', component: Login},
+    {path:'mes-reservations', component: MesReservations},
+    {path:'historique', component: Historique},
+];
