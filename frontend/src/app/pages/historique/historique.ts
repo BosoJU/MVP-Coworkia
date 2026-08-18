678c3b8d71586d1ede6e568f0ae3727c055baaf1
Boso <justine.boso@hotmail.com>
2026-08-18 13:32:05 +0200

    frontend : connexion page historiques aux datas

--- frontend/src/app/pages/historique/historique.ts
+++ frontend/src/app/pages/historique/historique.ts
@@ -1,9 +1,39 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { Reservation } from '../../models/reservation';
+import { ReservationService } from '../../services/reservation-service';
 
 @Component({
   selector: 'app-historique',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './historique.html',
   styleUrl: './historique.css',
 })
-export class Historique {}
+export class Historique implements OnInit{
+  reservationsFutures: Reservation[] = [];
+  reservationsPassees: Reservation[] = [];
+
+  constructor(private reservationService: ReservationService, private changeDetector: ChangeDetectorRef){}
+
+  ngOnInit(): void {
+    this.reservationService.getFutures().subscribe({
+      next: (data) =>  {
+        this.reservationsFutures = data;
+        this.changeDetector.detectChanges();
+      },
+      error: (err) => {
+        console.error(err);
+      }
+    });
+
+    this.reservationService.getHistorique().subscribe({
+      next:(data) => {
+        this.reservationsPassees = data;
+        this.changeDetector.detectChanges();
+      },
+      error: (err) => {
+        console.error(err);
+      }
+    });
+  }
+}
