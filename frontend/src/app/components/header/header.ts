e736db3768ce981630eacc4187b697b699582444
Boso <justine.boso@hotmail.com>
2026-08-18 15:27:49 +0200

    frontend : connexion du header aux différentes routes Ajout d'un guard pour la page de login

--- frontend/src/app/components/header/header.ts
+++ frontend/src/app/components/header/header.ts
@@ -1,9 +1,32 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { Auth } from '../../services/auth';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
-export class Header {}
+export class Header {
+  menuOuvert = false;
+
+  constructor(private auth: Auth, private router: Router){}
+
+  ouvrirMenu():void{
+    this.menuOuvert = !this.menuOuvert;
+  }
+
+  estConnecte(){
+    return this.auth.estConnecte();
+  }
+
+  estAdmin(){
+    return this.auth.estAdmin();
+  }
+
+  logout(){
+    this.auth.logout();
+    this.router.navigate(['/login']);
+  }
+}
--- frontend/src/app/guards/auth-guard.ts
+++ frontend/src/app/guards/auth-guard.ts
@@ -1,5 +1,5 @@
 import { inject } from "@angular/core";
-import { CanActivate, CanActivateFn, Router } from "@angular/router";
+import { CanActivateFn, Router } from "@angular/router";
 import { Auth } from "../services/auth";
 
 export const authGuard: CanActivateFn = () => {
@@ -11,4 +11,16 @@ export const authGuard: CanActivateFn = () => {
         return false;
     }
     return true
+}
+//Sans ce guard, on accède à la page login en étant connecté 
+export const loginGuard: CanActivateFn = () => {
+    const auth = inject(Auth);
+    const router = inject(Router);
+
+    if(!auth.estConnecte()){
+        return true
+    }
+
+    router.navigate(['/zones']);
+    return false;
 }
